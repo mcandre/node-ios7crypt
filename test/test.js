fd9798194b3a7389c0e5b8da04fed7a2b1d6ee6d
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-12-17 09:06:32 -0600

    update node

--- test/test.js
+++ test/test.js
@@ -7,11 +7,7 @@ describe('ios7crypt', function() {
     describe('reversible', function() {
         it('should be reversible', function() {
             function propReversible(password) {
-                if (password.length > 1) {
-                    return ios7crypt.decrypt(ios7crypt.encrypt(password)) === password;
-                }
-
-                return true;
+                return ios7crypt.decrypt(ios7crypt.encrypt(password)) === password;
             }
 
             fc.assert(fc.property(fc.string(), propReversible));
